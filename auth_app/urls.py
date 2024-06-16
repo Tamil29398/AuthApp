b3023bcf521326ecb28d2ffd6a83e3f1f60fb891
Tamil29398 <tamilselvan.muruganantham@outlook.com>
2024-06-16 14:46:10 +0530

    password reset Token created

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import AuthView
+from .views import AuthView,RefreshTokenView,PasswordResetTokenView
 
 urlpatterns=[
     path('token/',AuthView.as_view()),
+    path('refresh/',RefreshTokenView.as_view()),
+    path('reset_password/',PasswordResetTokenView.as_view())
+
+
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -4,6 +4,7 @@ from django.utils.timezone import datetime
 from django.contrib.auth.tokens import default_token_generator
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 from .models import AuthToken,ResetPasswordToken
 from datetime import timedelta
 # Create your views here.
@@ -29,5 +30,39 @@ class AuthView(APIView):
             'access':token.access_token,
             'refresh':token.refresh_token
             })
-
+class RefreshTokenView(APIView):
+    def post(self,request):
+        refresh_token=request.data.get('refresh','')
+        token=AuthToken.objects.filter(refresh_token=refresh_token,refresh_tok_valid__gt=datetime.now()).first()
+        if token is None:
+            return Response('Invalid Authentication',status=401)
+        token.save()
+        return Response({'access':token.access_token})
         
+class PasswordResetTokenView(APIView):
+    def get(self,request):
+        email=request.GET.get('email','')
+        if not email:
+            return Response('Email-id Requird')
+        user=User.objects.filter(email=email).first()
+        if user is None:
+            return Response('User Account not Found',status=400)
+        reset_token,_=ResetPasswordToken.objects.get_or_create(user=user)
+        reset_token.save()
+        return Response({
+            'reset_token':reset_token.reset_token
+        })
+        
+    def post(self,request):
+        reset_token=request.data.get('reset_token','')
+        password=request.data.get('password')
+
+        user=ResetPasswordToken.objects.filter(reset_token=reset_token).first()
+        if user is None:
+            return Response('Invalid Token')
+        user.user.set_password(password)
+        user.user.save()
+        return Response({'reset_password SuccessFully'})
+    
+    
+
